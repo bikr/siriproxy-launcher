b961c0b04da8c6390d5db2c1bfde8b253772af3d
bikr <bikr@bikr.net>
2011-12-12 12:14:54 -0500

    Update lib/siriproxy-launcher.rb

--- lib/siriproxy-launcher.rb
+++ lib/siriproxy-launcher.rb
@@ -24,7 +24,7 @@ class SiriProxy::Plugin::Launcher < SiriProxy::Plugin
  end
 
 
-listen_for /please fix/i do
+listen_for /please fix my media center/i do
 	te = Appscript.app('iTunes')
 	if te.is_running?
    	  te.quit
